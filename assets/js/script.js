23b8ac67d60016aac09da0268e28bbf7096df370
ThasNeto <thas.neto@gmail.com>
2023-05-17 11:53:10 -0300

    responsive and fixing a bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,9 @@ const relogio = setInterval(function time() {
 	let min = dateToday.getMinutes();
 	let seg = dateToday.getSeconds();
 
-	if (hr <= 10) {hr = "0" + hr};
-	if (min <= 10) {min = "0" + min};
-	if (seg <= 10) {seg= "0" + seg};
+	if (hr < 10) {hr = "0" + hr};
+	if (min < 10) {min = "0" + min};
+	if (seg < 10) {seg= "0" + seg};
 
 
 	horas.textContent = hr;
